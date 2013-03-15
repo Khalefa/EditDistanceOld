7da3fbc9c621e22f92302c21cec17a7d993fd491
khalefa <m.elbasiouny@gmail.com>
2013-03-15 23:40:32 +0200

    Not working

--- bloom_impl/core.cpp
+++ bloom_impl/core.cpp
@@ -1,17 +1,18 @@
-#include "../include/core.h"
+#include "core.h"
 #include <cstring>
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
-//#define WORD_LIST
+#include <map>
+#include <set>
+#define WORD_LIST
 using namespace std;
-
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 // Computes edit distance between a null-terminated string "a" with length "na"
 //  and a null-terminated string "b" with length "nb" 
-int EditDistance(char* a, int na, char* b, int nb)
+int EditDistance(const char* a, int na, const char* b, int nb)
 {
 	int oo=0x7FFFFFFF;
 
@@ -69,14 +70,14 @@ int EditDistance(char* a, int na, char* b, int nb)
 
 // Computes Hamming distance between a null-terminated string "a" with length "na"
 //  and a null-terminated string "b" with length "nb" 
-unsigned int HammingDistance(char* a, int na, char* b, int nb)
+unsigned int HammingDistance(const char* a, int na, const char* b, int nb)
 {
 	int j, oo=0x7FFFFFFF;
 	if(na!=nb) return oo;
-	
+
 	unsigned int num_mismatches=0;
 	for(j=0;j<na;j++) if(a[j]!=b[j]) num_mismatches++;
-	
+
 	return num_mismatches;
 }
 
@@ -89,6 +90,9 @@ struct Query
 	char str[MAX_QUERY_LENGTH];
 	MatchType match_type;
 	unsigned int match_dist;
+#ifdef WORD_LIST
+	vector<unsigned int> words;
+#endif
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -109,22 +113,24 @@ vector<Query> queries;
 // Keeps all currently available results that has not been returned yet
 vector<Document> docs;
 
+#ifdef WORD_LIST
 //a flat word list
 typedef struct _word{
 	char str[MAX_WORD_LENGTH+1];
 	vector <int> queries_id;
 } Word;
 
-vector <Word> words;
+vector<Word> words;
 
 int findInwords(char* str){
 	for (int i=0;i< words.size() ;  i++){
-	  Word *w=&words[i];
-	     if(strcmp(w->str, str)==0) 
-		return i;
+		Word *w=&words[i];
+		if(strcmp(w->str, str)==0) 
+			return i;
 	}
 	return -1;
 }
+#endif
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 ErrorCode InitializeIndex(){return EC_SUCCESS;}
@@ -142,34 +148,38 @@ ErrorCode StartQuery(QueryID query_id, const char* query_str, MatchType match_ty
 	strcpy(query.str, query_str);
 	query.match_type=match_type;
 	query.match_dist=match_dist;
+#ifdef WORD_LIST
+	int iq=0;
+	while(query.str[iq]){
+		while(query.str[iq]==' ') iq++;
+		if(!query.str[iq]) break;
+		char* qword=&query.str[iq];
+
+		int lq=iq;
+		while(query.str[iq] && query.str[iq]!=' ') iq++;
+		char qt=query.str[iq];
+		query.str[iq]=0;
+		lq=iq-lq;
+
+		//check if qword is already there
+		int  i=findInwords(qword);
+		if (i>=0){
+			words[i].queries_id.push_back(query_id);
+			query.words.push_back(i);
+		} else {
+			Word w;
+			strcpy(w.str,qword);
+			w.queries_id.push_back(query_id);
+			words.push_back(w);
+			query.words.push_back(findInwords(qword));
+		}
+		printf("i%d %s\n",findInwords(qword), qword);
+		query.str[iq]=qt;
+	}
+#endif	
 	// Add this query to the active query set
 	queries.push_back(query);
-#ifdef WORD_LIST
-      	int iq=0;
-	while(query.str[iq]){
-	   while(query.str[iq]==' ') iq++;
-	   if(!query.str[iq]) break;
-	   char* qword=&query.str[iq];
-        
-           int lq=iq;
-	   while(query.str[iq] && query.str[iq]!=' ') iq++;
-	   char qt=query.str[iq];
-	   query.str[iq]=0;
-	   lq=iq-lq;
-
-           //check if qword is there
-           int  i=findInwords(qword);
-           if (i>=0){
-		words[i].queries_id.push_back(query_id);
-           } else {
- 	          Word w;
- 	          strcpy(w.str,qword);
- 	          w.queries_id.push_back(query_id);
-		  words.push_back(w);
-	   }
-           query.str[iq]=qt;
-        }
-#endif	
+printf("Query %d words %d\n", query.query_id, query.words.size());
 	return EC_SUCCESS;
 }
 
@@ -178,6 +188,7 @@ ErrorCode StartQuery(QueryID query_id, const char* query_str, MatchType match_ty
 ErrorCode EndQuery(QueryID query_id)
 {
 	// Remove this query from the active query set
+	printf("Del Query %d\n",query_id);
 	unsigned int i, n=queries.size();
 	for(i=0;i<n;i++)
 	{
@@ -189,7 +200,7 @@ ErrorCode EndQuery(QueryID query_id)
 	}
 	for(i=0;i<n;i++)
 
-	return EC_SUCCESS;
+		return EC_SUCCESS;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -204,90 +215,129 @@ int GetWordValue(const char * s, int n){
 	}
 	return value;
 }
+int MatchWord(const char *dword, const char *qword, int ld, int lq, int dword_value, const Query * quer){
+	int matching_word=false;
 
+	int qword_value=GetWordValue(qword,lq);            	
+	int delta=dword_value-qword_value;
+	if(delta<0)delta=-delta;
+	if(delta<=26*quer->match_dist){
+		if(quer->match_type==MT_EXACT_MATCH)
+		{
+			if(ld==lq) //same length
+				if(strcmp(qword, dword)==0) matching_word=true;
+		}
+		else if(quer->match_type==MT_HAMMING_DIST)
+		{
+			if(ld==lq){ 						
+				unsigned int num_mismatches=HammingDistance(qword, lq, dword, ld);
+				if(num_mismatches<=quer->match_dist) matching_word=true;
+			}
+		}
+		else if(quer->match_type==MT_EDIT_DIST)
+		{
+			unsigned int edit_dist=EditDistance(qword, lq, dword, ld);
+			if(edit_dist<=quer->match_dist) matching_word=true;
+		}
+	}
+
+	return matching_word;
+}
 ErrorCode MatchDocument(DocID doc_id, const char* doc_str)
 {
 	char cur_doc_str[MAX_DOC_LENGTH];
 	strcpy(cur_doc_str, doc_str); //I really want to remove this
+	vector<unsigned int> query_ids;
+	set <unsigned int> foundWords;
 
 	unsigned int i, n=queries.size();
-	vector<unsigned int> query_ids;
-
-	// Iterate on all active queries to compare them with this new document
+#if 1	
+	map<unsigned int,int> lqueries;
 	for(i=0;i<n;i++)
 	{
-		bool matching_query=true;
 		Query* quer=&queries[i];
-
+		int qid=quer->query_id;	
+		int q_length=0;
 		int iq=0;
-		while(quer->str[iq] && matching_query)
+		while(quer->str[iq] )
 		{
 			while(quer->str[iq]==' ') iq++;
 			if(!quer->str[iq]) break;
-			char* qword=&quer->str[iq];
 
 			int lq=iq;
 			while(quer->str[iq] && quer->str[iq]!=' ') iq++;
-			char qt=quer->str[iq];
-			quer->str[iq]=0;
 			lq=iq-lq;
-			
-			int qword_value=GetWordValue(qword,lq);            
-			bool matching_word=false;
-
-			int id=0;
-			while(cur_doc_str[id] && !matching_word)
-			{
-				while(cur_doc_str[id]==' ') id++;
-				if(!cur_doc_str[id]) break;
-				char* dword=&cur_doc_str[id];
-
-				int ld=id;
-				while(cur_doc_str[id] && cur_doc_str[id]!=' ') id++;
-				char dt=cur_doc_str[id];
-				cur_doc_str[id]=0;
-
-				ld=id-ld;
-				int dword_value=GetWordValue(dword,ld);
-				int delta=dword_value-qword_value;
-				if(delta<0)delta=-delta;
-				if(delta<=26*quer->match_dist){
-					if(quer->match_type==MT_EXACT_MATCH)
-					{
-						if(ld==lq) //same length
-							if(strcmp(qword, dword)==0) matching_word=true;
-					}
-					else if(quer->match_type==MT_HAMMING_DIST)
-					{
-						unsigned int num_mismatches=HammingDistance(qword, lq, dword, ld);
-						if(num_mismatches<=quer->match_dist) matching_word=true;
-					}
-					else if(quer->match_type==MT_EDIT_DIST)
-					{
-						unsigned int edit_dist=EditDistance(qword, lq, dword, ld);
-						if(edit_dist<=quer->match_dist) matching_word=true;
-					}
-				}
-
-				cur_doc_str[id]=dt;
-			}
-
-			quer->str[iq]=qt;
-
-			if(!matching_word)
-			{
-				// This query has a word that does not match any word in the document
-				matching_query=false;
-			}
+			q_length+=lq;
 		}
+		lqueries[qid]=q_length;
+	}	
+#endif
+	// Start scaning the document
+	int id=0;
+	while(cur_doc_str[id])
+	{
+		while(cur_doc_str[id]==' ') id++;
+		if(!cur_doc_str[id]) break;
+		char* dword=&cur_doc_str[id];
 
-		if(matching_query)
+		int ld=id;
+		while(cur_doc_str[id] && cur_doc_str[id]!=' ') id++;
+		char dt=cur_doc_str[id];
+		cur_doc_str[id]=0;
+		ld=id-ld;
+
+		int dword_value=GetWordValue(dword,ld);
+
+		for(i=0;i<n ;i++)
 		{
-			// This query matches the document
-			query_ids.push_back(quer->query_id);
+			int qid =queries[i].query_id;
+			if (lqueries[qid]<=0)continue;
+
+			Query* quer=&queries[i];
+
+			int iq=0;
+			while(quer->str[iq])
+			{
+				int matching_word=false;
+
+				while(quer->str[iq]==' ') iq++;
+				if(!quer->str[iq]) break;
+				char* qword=&quer->str[iq];
+
+				int lq=iq;
+				while(quer->str[iq] && quer->str[iq]!=' ') iq++;
+				char qt=quer->str[iq];
+				quer->str[iq]=0;
+				lq=iq-lq;
+				matching_word=MatchWord(dword,qword,  ld,  lq, dword_value, quer);
+				if (matching_word){
+					// update statisics
+					lqueries[qid]-=lq;
+					int i=findInwords(qword); 
+					foundWords.insert(i);
+				}
+				quer->str[iq]=qt;
+			}
 		}
+		cur_doc_str[id]=dt;
 	}
 
+	for(int i=0;i<n;i++){
+		unsigned int qid=queries[i].query_id;
+		if(lqueries[qid]<=0)  {
+			int to_insert=1;
+			//make sure all words have been found
+			int nn=queries[i].words.size();
+			for(int j=0;j<nn;j++){
+				if (foundWords.find(queries[i].words[j])==foundWords.end())
+					{to_insert=0; break;}
+				
+			}
+			if(to_insert)
+				query_ids.push_back(qid);
+
+		}
+	}
 	Document doc;
 	doc.doc_id=doc_id;
 	doc.num_res=query_ids.size();
